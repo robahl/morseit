c2ee7344793476f4b671a74bfe723943f529c0fa
Robert Ahlberg <rob.ahlberg@gmail.com>
2019-12-08 00:36:33 +0100

    First working version

--- MorseIt/ViewController.swift
+++ MorseIt/ViewController.swift
@@ -9,18 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
+  
+  var morseSequence = [String]()
+  var signalDownTime: TimeInterval = 0.0
+  var signalUpTime: TimeInterval = 0.0
+  let dotDuration = 0.12 // 150 ms
+  
+  let morseCodes = [
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z"
+  ]
+  @IBOutlet weak var messageLabel: UILabel!
 
   @IBAction func signalTouchedDown(_ sender: Any) {
-    print("Down")
+    signalDownTime = Date().timeIntervalSince1970
+    if (signalDownTime - signalUpTime) > (dotDuration * 5) {
+      morseSequence.append(" ")
+    }
   }
   
   @IBAction func signalTouchedUp(_ sender: Any) {
-    print("Up")
+    signalUpTime = Date().timeIntervalSince1970
+    let duration = signalUpTime - signalDownTime
+    morseSequence.append(morseSignal(duration))
+    
+    messageLabel.text = morseSequenceToAlpha(morseSequence)
+  }
+  
+  
+  @IBAction func clearMessage(_ sender: Any) {
+    messageLabel.text = ""
+    morseSequence = []
+  }
+  
+  func morseSignal(_ duration: TimeInterval) -> String {
+    return duration >= 3 * dotDuration ? "-" : "."
+  }
+  
+  func morseSequenceToAlpha(_ sequence: [String]) -> String {
+    let morseCharacters = sequence.split(separator: " ").map({(character) -> String in
+      return character.joined(separator: "")
+    })
+    
+    return morseCharacters.map({(character) -> String in
+      return (morseCodes[character] ?? "")
+      }).joined(separator: "")
   }
 }
 
